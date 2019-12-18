cac1b2c96746cc395315b4514ab61b52b76bd87f
dariav01 <47599038+dariav01@users.noreply.github.com>
2019-12-18 13:18:37 +0100

    Add files via upload

--- practica_final19.c
+++ practica_final19.c
@@ -10,6 +10,34 @@
 
 /*Declaracion de las variables globales*/
 FILE *Fichero;
-String ColaSolicitudes[15];
+String colaSolicitudes[15];
+String colaSocial=15;
+String usuarios[15];
+String contador_Solicitudes;
+String id;
+bool atndido;
+String tipo;
+String usuarios_Actividad[4];
+bool Atendedores;
+FILE *logFile;
 
+int main(){
+	String Invitacion;
+	struct signal ss;
+	switch (fork())
+	{
+		case -1:
+			perror("fork");
+			return 1;
+		case 0:
+			for(;;)
+				pause();
+		default:
+			ss.sa_handler;
+			if(-1==signal(SIGUSR1, &ss, NULL))
 
+	}
+		
+	
+
+}
