a2f799465452a30e77467793298e0aa8d11f142a
Ricardo Alonso <47599651+Ralonp03@users.noreply.github.com>
2019-12-18 11:53:41 +0100

    Add files via upload

---
+++ practica_final19.c
@@ -0,0 +1,15 @@
+/*Añadimos bibliotecas*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys.types.h>
+#include <sys.wait.h>
+#include <string.h>
+#include <pthread.h>
+
+
+/*Declaracion de las variables globales*/
+FILE *Fichero;
+String ColaSolicitudes[15];
+
+
